1cef4c0a3870d04812298cec89794dd5282024bc
Zeroscapez <ultlomini@gmail.com>
2026-04-04 05:02:03 -0400

    Add enemy assets, scripts and TestScene
    Add player shooting system with spherecast and billboard enemies
    
    - Add GunControl with New Input System integration and fire rate limiting
    - Add IShootable interface for extensible hit detection
    - Replace raycast with spherecast for lenient hit detection
    - Add debug sphere visualization for spherecast in Scene view
    - Add FlashOnHit component for visual hit feedback on enemies
    - Created Test enemy sprite

---
+++ Assets/Scripts/EnemyTarget.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class EnemyTarget : MonoBehaviour, IShootable
+{
+
+    public EnemyData enemyData;
+    public int CurrentEnemyHealth;
+    private string enemyName;
+    private int maxEnemyHealth;
+
+    [Header("VFX / Feedback")]
+    public Color FlashColor = Color.red;
+    public float FlashDuration = 0.1f;
+    private Material _material;
+    private Color _originalColor;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _material = GetComponent<Renderer>().material;
+        if (_material)
+        {
+            _originalColor = _material.color;
+        }
+
+
+        if (enemyData == null)
+        {
+            Debug.LogError("EnemyData not assigned on " + gameObject.name);
+            Debug.LogError("Assigning default values to prevent errors.");
+
+            enemyName = "Default Enemy";
+            maxEnemyHealth = 5;
+            CurrentEnemyHealth = maxEnemyHealth;
+            return;
+        }
+        enemyName = enemyData.EnemyName;
+        maxEnemyHealth = enemyData.MaxHealth;
+        CurrentEnemyHealth = maxEnemyHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void OnHit()
+    {
+        Debug.Log("Enemy hit! = " + enemyName + " Health: " + CurrentEnemyHealth + "/" + maxEnemyHealth);
+        CurrentEnemyHealth--;
+
+        if (CurrentEnemyHealth <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            StopAllCoroutines(); // Prevents overlapping flashes if hit rapidly
+            StartCoroutine(Flash());
+        }
+    }
+
+    public void Die()
+    {
+        if (GameManager.Instance.GAME == GameState.Debug)
+        {
+            Debug.Log(enemyName + " has been defeated!");
+            return;
+        }
+        this.gameObject.SetActive(false);
+    }
+
+    IEnumerator Flash()
+    {
+        _material.color = FlashColor;
+        yield return new WaitForSeconds(FlashDuration);
+        _material.color = _originalColor;
+    }
+}
